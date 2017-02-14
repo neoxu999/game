6021960bad772c7405ae345214d0f8b1501fb046
neoxu999 <neoxu999@gmail.com>
2017-02-15 10:09:49 +1100

    Update CustomSpringContextLoaderListener.java

--- src/main/java/com/littlepay/game/listener/CustomSpringContextLoaderListener.java
+++ src/main/java/com/littlepay/game/listener/CustomSpringContextLoaderListener.java
@@ -1,13 +1,7 @@
 /******************************************************
- * Copyright (c) Quant Pty. Ltd. [2015-2017]
- * Copyright subsists in this code. 
- * Except as permitted under the Copyright Act 1968 (Cth), no part of this 
- * work may be reproduced, published or adapted in any way, without the 
- * specific written permission of Quant Pty. Ltd. All rights reserved.
- *  
  * 
- * RabbitMQListener.java - com.datacenter.listener
- * @author Long Xu, 1Feb.,2017
+ * RabbitMQListener.java - com.littlepay.game.listener
+ * @author Long Xu, 17Feb.,2017
 ******************************************************/
 package com.littlepay.game.listener;
 
