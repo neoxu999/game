3939a03aced85d192faad833dac39e4fbd660c11
neoxu999 <neoxu999@gmail.com>
2017-02-15 10:07:29 +1100

    Update JedisFactory.java

--- src/main/java/com/littlepay/game/service/JedisFactory.java
+++ src/main/java/com/littlepay/game/service/JedisFactory.java
@@ -34,15 +34,11 @@ public class JedisFactory {
 //	// if set -1，not limitation； If the pool allocate maxActive jedis instance，the pool status exhausted。
 //	private static int MAX_ACTIVE = 1024;
 
-	// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+	// pool idle jedis instance，default 8.
 	private static int MAX_IDLE = 200;
 
-//	// 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
-//	private static int MAX_WAIT = 10000;
-
 	private static int TIMEOUT = 10000;
-
-	// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+	
 	private static boolean TEST_ON_BORROW = true;
 
 	private static JedisPool jedisPool = null;
